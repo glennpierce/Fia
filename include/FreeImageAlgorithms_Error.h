a41130f8ed989b9853d147996f9a63d1b76d8f15
pierce <>
2006-06-28 02:30:28 +0100

    Export error functions as c symbols.

--- include/FreeImageAlgorithms_Error.h
+++ include/FreeImageAlgorithms_Error.h
@@ -1,6 +1,10 @@
 #ifndef __FREEIMAGE_ALGORITHMS_ERROR__
 #define __FREEIMAGE_ALGORITHMS_ERROR__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "FreeImageAlgorithms.h"
 
 typedef void (*FreeImageAlgorithms_OutputMessageFunction)(const char *msg);
@@ -11,6 +15,9 @@ FreeImageAlgorithms_SetOutputMessage(FreeImageAlgorithms_OutputMessageFunction o
 DLL_API void DLL_CALLCONV
 FreeImageAlgorithms_SendOutputMessage(const char *message);
 
+#ifdef __cplusplus
+}
+#endif
 
 #endif
 
