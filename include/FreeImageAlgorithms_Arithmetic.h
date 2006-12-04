106d39b5595820883bd2c3bfe94c5aced7f9d53f
pierce <>
2006-12-04 18:37:23 +0100

    Added documentation for statistic function and arithmetic functions. Update template files.

--- include/FreeImageAlgorithms_Arithmetic.h
+++ include/FreeImageAlgorithms_Arithmetic.h
@@ -7,39 +7,114 @@
 extern "C" {
 #endif
 
+/*! \file 
+	Provides various arithmetic methods.
+*/ 
+
+/** \brief Transpose an image.
+ *
+ *  This function transposes the image data. Ie its row and columns are swapped.
+ *
+ *  \param src FIBITMAP bitmap to transpose.
+*/
 DLL_API FIBITMAP* DLL_CALLCONV
 FreeImageAlgorithms_Transpose(FIBITMAP *src);
 
+/** \brief Return the log image.
+ *
+ *  This function returns an image where the log of each pixel is taken.
+ *
+ *  \param src FIBITMAP bitmap to perform the log operation on.
+*/
 DLL_API FIBITMAP* DLL_CALLCONV
 FreeImageAlgorithms_Log(FIBITMAP *src);
 
+/** \brief Multiply two greylevel images.
+ *
+ *  \param dst FIBITMAP first bitmap to perform the multiply this also serves as the output.
+ *  \param src FIBITMAP second bitmap to perform the multiply operation on.
+ *  \return int FREEIMAGE_ALGORITHMS_SUCCESS on success or FREEIMAGE_ALGORITHMS_ERROR on error.
+*/
 DLL_API int DLL_CALLCONV 
 FreeImageAlgorithms_MultiplyGreyLevelImages(FIBITMAP* dst, FIBITMAP* src);
 
+/** \brief Divide two greylevel images.
+ *
+ *  \param dst FIBITMAP first bitmap to perform the divide this also serves as the output.
+ *  \param src FIBITMAP second bitmap to perform the divide operation on.
+ *  \return int FREEIMAGE_ALGORITHMS_SUCCESS on success or FREEIMAGE_ALGORITHMS_ERROR on error.
+*/
 DLL_API int DLL_CALLCONV 
 FreeImageAlgorithms_DivideGreyLevelImages(FIBITMAP* dst, FIBITMAP* src);
 
+/** \brief Add two greylevel images.
+ *
+ *  \param dst FIBITMAP first bitmap to perform the add this also serves as the output.
+ *  \param src FIBITMAP second bitmap to perform the add operation on.
+ *  \return int FREEIMAGE_ALGORITHMS_SUCCESS on success or FREEIMAGE_ALGORITHMS_ERROR on error.
+*/
 DLL_API int DLL_CALLCONV 
 FreeImageAlgorithms_AddGreyLevelImages(FIBITMAP* dst, FIBITMAP* src);
 
+/** \brief Subtract two greylevel images.
+ *
+ *  \param dst FIBITMAP first bitmap to perform the subtract this also serves as the output.
+ *  \param src FIBITMAP second bitmap to perform the subtract operation on.
+ *  \return int FREEIMAGE_ALGORITHMS_SUCCESS on success or FREEIMAGE_ALGORITHMS_ERROR on error.
+*/
 DLL_API int DLL_CALLCONV 
 FreeImageAlgorithms_SubtractGreyLevelImages(FIBITMAP* dst, FIBITMAP* src);
 
+/** \brief Multiply a greylevel image by a constant.
+ *
+ *  \param dst FIBITMAP bitmap to perform the multiply this also serves as the output.
+ *  \param constant Constant used to perform the multiply.
+ *  \return int FREEIMAGE_ALGORITHMS_SUCCESS on success or FREEIMAGE_ALGORITHMS_ERROR on error.
+*/
 DLL_API int DLL_CALLCONV 
 FreeImageAlgorithms_MultiplyGreyLevelImageConstant(FIBITMAP* dst, double constant);
 
+/** \brief Divide a greylevel image by a constant.
+ *
+ *  \param dst FIBITMAP bitmap to perform the divide this also serves as the output.
+ *  \param constant Constant used to perform the divide.
+ *  \return int FREEIMAGE_ALGORITHMS_SUCCESS on success or FREEIMAGE_ALGORITHMS_ERROR on error.
+*/
 DLL_API int DLL_CALLCONV 
 FreeImageAlgorithms_DivideGreyLevelImageConstant(FIBITMAP* dst, double constant);
 
+/** \brief Add a greylevel image by a constant.
+ *
+ *  \param dst FIBITMAP bitmap to perform the add this also serves as the output.
+ *  \param constant Constant used to perform the add.
+ *  \return int FREEIMAGE_ALGORITHMS_SUCCESS on success or FREEIMAGE_ALGORITHMS_ERROR on error.
+*/
 DLL_API int DLL_CALLCONV 
 FreeImageAlgorithms_AddGreyLevelImageConstant(FIBITMAP* dst, double constant);
 
+/** \brief Subtract a greylevel image by a constant.
+ *
+ *  \param dst FIBITMAP bitmap to perform the subtract this also serves as the output.
+ *  \param constant Constant used to perform the subtract.
+ *  \return int FREEIMAGE_ALGORITHMS_SUCCESS on success or FREEIMAGE_ALGORITHMS_ERROR on error.
+*/
 DLL_API int DLL_CALLCONV 
 FreeImageAlgorithms_SubtractGreyLevelImageConstant(FIBITMAP* dst, double constant);
 
+/** \brief Calculate the complex conjugate of a complex image.
+ *
+ *  \param src FIBITMAP bitmap must be of type complex.
+ *  \return int FREEIMAGE_ALGORITHMS_SUCCESS on success or FREEIMAGE_ALGORITHMS_ERROR on error.
+*/
 DLL_API int DLL_CALLCONV 
 FreeImageAlgorithms_ComplexConjugate(FIBITMAP* src);
 
+/** \brief Multiply two complex images.
+ *
+ *  \param dst FIBITMAP first bitmap to perform the multiply this also serves as the output.
+ *  \param src FIBITMAP second bitmap to perform the multiply operation on.
+ *  \return int FREEIMAGE_ALGORITHMS_SUCCESS on success or FREEIMAGE_ALGORITHMS_ERROR on error.
+*/
 DLL_API int DLL_CALLCONV 
 FreeImageAlgorithms_MultiplyComplexImages(FIBITMAP* dst, FIBITMAP* src);
 
--- include/FreeImageAlgorithms_Statistics.h
+++ include/FreeImageAlgorithms_Statistics.h
@@ -7,42 +7,98 @@
 extern "C" {
 #endif
 
+/*! \file 
+	Provides various statistical methods for FIBITMAP's.
+*/ 
+
+/** \brief Equalise and image using histogram equalisation with random additions.
+ *
+ *  \param src FIBITMAP bitmap to perform the equalisation operation on.
+ *  \return FIBITMAP on success or NULL on error.
+*/
 DLL_API FIBITMAP* DLL_CALLCONV
 FreeImageAlgorithms_HistEq_Random_Additions(FIBITMAP *src);
 
+/** \brief Equalise and image using histogram equalisation.
+ *
+ *  \param src FIBITMAP bitmap to perform the equalisation operation on.
+ *  \return FIBITMAP on success or NULL on error.
+*/
 DLL_API FIBITMAP* DLL_CALLCONV
 FreeImageAlgorithms_HistEq(FIBITMAP *src);
 
+/** \brief Calculate the greylevel average of all the pixels.
+ *
+ *  \param src FIBITMAP bitmap to perform the average calculation operation on. 
+ *	           Must be a greylevel image.
+ *  \return double Average value on success or 0.0 on error.
+*/
 DLL_API double DLL_CALLCONV
 FreeImageAlgorithms_GetGreyLevelAverage(FIBITMAP *src);
 
+/** \brief Return the histogram for a greylevel image.
+ *
+ *	This function is different from the FreeImage_GetHist as you can specify how
+ *  to bin values.
+ *
+ *  \param src FIBITMAP bitmap to perform the histogram operation on.
+ *  \param min The minimum value where binning or histogram counting begins.
+ *  \param max The maximum value where binning or histogram counting ends.
+ *  \param number_of_bins How many bins you want between min and max.
+ *  \param hist Long pointer to the histogram data.
+ *  \return int FREEIMAGE_ALGORITHMS_SUCCESS on success or FREEIMAGE_ALGORITHMS_ERROR on error.
+*/
 DLL_API int DLL_CALLCONV
 FreeImageAlgorithms_Histogram(FIBITMAP *src, double min, double max, int number_of_bins, unsigned long *hist);
 
+/** \brief Return the histogram for a rgb image.
+ *
+ *	This function is different from the FreeImage_GetHist as you can specify how
+ *  to bin values.
+ *
+ *  \param src FIBITMAP bitmap to perform the histogram operation on.
+ *  \param min The minimum value where binning or histogram counting begins.
+ *  \param max The maximum value where binning or histogram counting ends.
+ *  \param number_of_bins How many bins you want between min and max.
+ *  \param rhist Long pointer to the red histogram data.
+ *  \param ghist Long pointer to the green histogram data.
+ *  \param bhist Long pointer to the blue histogram data.
+ *  \return int FREEIMAGE_ALGORITHMS_SUCCESS on success or FREEIMAGE_ALGORITHMS_ERROR on error.
+*/
 DLL_API int DLL_CALLCONV
 FreeImageAlgorithms_RGBHistogram(FIBITMAP *src, double min, double max, int number_of_bins,
 			unsigned long *rhist, unsigned long *ghist, unsigned long *bhist);
 
 
-/*
- * This function finds the the amount of white ie area in a monochrome image.
- * This works with 8 bit images by assuming everything above 1 is white.
+/** \brief This function finds the the amount of white ie area in a monochrome image.
+ *		   This works with 8 bit images by assuming everything above 1 is white.
+ *  \param src FIBITMAP bitmap to perform the histogram operation on.
+ *  \param white_area unsigned int * Counts of pixels above or equal to 1.
  */
 DLL_API int DLL_CALLCONV
 FreeImageAlgorithms_MonoImageFindWhiteArea(FIBITMAP *src, unsigned int *white_area);
 
-/*
- * This function finds the the amount of white ie area in a monochrome image.
- * This works with 8 bit images by assuming everything above 1 is white.
+/** \brief This function finds the the amount of white ie area in a monochrome image.
+ *		   This works with 8 bit images by assuming everything above 1 is white.
+ *  \param src FIBITMAP bitmap to perform the histogram operation on.
+ *  \param white_area double * Fraction of pixels above or equal to 1.
+ *  \param black_area double * Fraction of pixels below 1.
+ *  \return int FREEIMAGE_ALGORITHMS_SUCCESS on success or FREEIMAGE_ALGORITHMS_ERROR on error.
  */
 DLL_API int DLL_CALLCONV
 FreeImageAlgorithms_MonoImageFindWhiteFraction(FIBITMAP *src, double *white_area, double *black_area);
 
-// This function determines how a detail is present though two images.
-// tp (True Positive) A detail present in src is also in result.
-// tn (True Negative) A detail not in src is not in result ie two pixels that are 0.
-// fp (False Positive) A detail not in src is in result.
-// fn (False Negative) A detail in src is not in result.
+/** \brief This function determines how a detail is present though two images.
+ *
+ *  \param src FIBITMAP bitmap to perform the comparison on.
+ *  \param result FIBITMAP bitmap to perform the comparison on. This is the expected result image ie
+ *						  gold standard.
+ *  \param tp int * (True Positive) A detail present in src is also in result.
+ *  \param tn int * (True Negative) A detail not in src is not in result ie two pixels that are 0.
+ *  \param fp int * (False Positive) A detail not in src is in result.
+ *  \param fn int * (False Negative) A detail in src is not in result.
+ *  \return int FREEIMAGE_ALGORITHMS_SUCCESS on success or FREEIMAGE_ALGORITHMS_ERROR on error.
+ */
 DLL_API int DLL_CALLCONV
 FreeImageAlgorithms_MonoTrueFalsePositiveComparison(FIBITMAP *src, FIBITMAP *result,
 													int *tp, int *tn, int *fp, int *fn);
