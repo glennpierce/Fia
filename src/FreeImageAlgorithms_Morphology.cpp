39de005d5d8bdae66f8118fdcb24fb754b7e7d40
Glenn Pierce <glennpierce@gmail.com>
2007-10-19 22:25:56 +0100

    Removed generated docs. Added lgpl license to files before release.

--- src/FreeImageAlgorithms_Morphology.cpp
+++ src/FreeImageAlgorithms_Morphology.cpp
@@ -1,3 +1,20 @@
+Copyright 2007 Glenn Pierce
+
+This file is part of FreeImageAlgorithms.
+
+FreeImageAlgorithms is free software: you can redistribute it and/or modify
+it under the terms of the Lesser GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FreeImageAlgorithms is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+Lesser GNU General Public License for more details.
+
+You should have received a copy of the Lesser GNU General Public License
+along with FreeImageAlgorithms.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "FreeImageAlgorithms.h"
 #include "FreeImageAlgorithms_Convolution.h"
 #include "FreeImageAlgorithms_Convolution_Private.h"
